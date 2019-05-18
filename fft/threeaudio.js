ece7cda2e4e7b15e359c3e8c558cb18ade947c43
mtoutside <more.humor.or@gmail.com>
2019-05-18 19:45:12 +0900

    change dimention ZY

--- fft/threeaudio.js
+++ fft/threeaudio.js
@@ -1,8 +1,8 @@
 (() => {
 	// variables
-	let canvasWidth  = null;
-	let canvasHeight = null;
-	let targetDOM    = null;
+	let		canvasWidth  = window.innerWidth;
+	let		canvasHeight = window.innerHeight;
+	let		targetDOM    = document.getElementById('webgl');
 	let run = true;
 	let isDown = false;
 	// three objects
@@ -11,100 +11,129 @@
 	let controls;
 	let renderer;
 	let analyser;
+	let box = [];
 	let geometry;
 	let material;
 	let mesh;
-	const fftSize = 1024;
-
+	const fftSize = 32;
 	let axesHelper;
 	// constant variables
+	const CAMERA_PARAM = {
+		fovy: 90,
+		aspect: canvasWidth / canvasHeight,
+		near: 0.1,
+		far: 50.0,
+		x: 0.0,
+		y: 2.0,
+		z: 9.0,
+		lookAt: new THREE.Vector3(0.0, 0.0, 0.0)
+	};
+
 	const RENDERER_PARAM = {
-			clearColor: 0x336633
+			clearColor: 0x333333
 	};
 
 
 	// entry point
 	window.addEventListener('load', () => {
-			// canvas
-			canvasWidth  = window.innerWidth;
-			canvasHeight = window.innerHeight;
-			targetDOM    = document.getElementById('webgl');
 
-			// scene and camera
-			scene = new THREE.Scene();
-			camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 50.0);
-			camera.position.x = 0.0;
-			camera.position.y = 10.0;
-			camera.position.z = 5.0;
-			camera.lookAt(new THREE.Vector3(0.0, 0.0, 0.0));
+		// scene and camera
+		scene = new THREE.Scene();
 
-			// renderer
-			renderer = new THREE.WebGLRenderer();
-			// renderer.setClearColor(new THREE.Color(RENDERER_PARAM.clearColor));
-			renderer.setSize(canvasWidth, canvasHeight);
-			targetDOM.appendChild(renderer.domElement);
+		camera = new THREE.PerspectiveCamera(
+			CAMERA_PARAM.fovy,
+			CAMERA_PARAM.aspect,
+			CAMERA_PARAM.near,
+			CAMERA_PARAM.far
+		);
+		camera.position.x = CAMERA_PARAM.x;
+		camera.position.y = CAMERA_PARAM.y;
+		camera.position.z = CAMERA_PARAM.z;
+		camera.lookAt(CAMERA_PARAM.lookAt);
 
-			// create an AudioListener and add it to the camera
-var listener = new THREE.AudioListener();
-camera.add( listener );
+		// renderer
+		renderer = new THREE.WebGLRenderer();
+		// renderer.setClearColor(new THREE.Color(RENDERER_PARAM.clearColor));
+		renderer.setSize(canvasWidth, canvasHeight);
+		targetDOM.appendChild(renderer.domElement);
 
-// create a global audio source
-var sound = new THREE.Audio( listener );
+		// create an AudioListener and add it to the camera
+		var listener = new THREE.AudioListener();
+		camera.add( listener );
 
-// load a sound and set it as the Audio object's buffer
-var audioLoader = new THREE.AudioLoader();
-audioLoader.load( './3_880mm.MP3', function( buffer ) {
-	sound.setBuffer( buffer );
-	sound.setLoop( false );
-	sound.setVolume( 0.5 );
-	sound.play();
-});
+		// create a global audio source
+		var sound = new THREE.Audio( listener );
 
-// create an AudioAnalyser, passing in the sound and desired fftSize
-analyser = new THREE.AudioAnalyser( sound, fftSize );
+		// load a sound and set it as the Audio object's buffer
+		var audioLoader = new THREE.AudioLoader();
+		audioLoader.load( './3_880mm.MP3', function( buffer ) {
+			sound.setBuffer( buffer );
+			sound.setLoop( false );
+			sound.setVolume( 0.5 );
+			sound.play();
+		});
+
+		// create an AudioAnalyser, passing in the sound and desired fftSize
+		analyser = new THREE.AudioAnalyser( sound, fftSize );
 
 
-uniforms = {
-	tAudioData: { value: new THREE.DataTexture( analyser.data, fftSize / 2, 1, THREE.LuminanceFormat ) }
-};
-material = new THREE.ShaderMaterial( {
-	uniforms: uniforms,
-	vertexShader: document.getElementById( 'vertexShader' ).textContent,
-	fragmentShader: document.getElementById( 'fragmentShader' ).textContent
-} );
-geometry = new THREE.PlaneBufferGeometry( 1, 1 );
-mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+		uniforms = {
+			tAudioData: { value: new THREE.DataTexture( analyser.data, fftSize / 2, 1, THREE.LuminanceFormat ) }
+		};
+		material = new THREE.ShaderMaterial( {
+			uniforms: uniforms,
+			vertexShader: document.getElementById( 'vertexShader' ).textContent,
+			fragmentShader: document.getElementById( 'fragmentShader' ).textContent
+		} );
+		geometry = new THREE.PlaneBufferGeometry( 1, 1 );
+		mesh = new THREE.Mesh( geometry, material );
+		// scene.add( mesh );
 
-			// events
-			window.addEventListener('keydown', (eve) => {
-					run = eve.key !== 'Escape';
-					if(eve.key === ' '){
-							isDown = true;
-					}
-			}, false);
-			window.addEventListener('keyup', (eve) => {
-					if(eve.key === ' '){
-							isDown = false;
-					}
-			}, false);
-			window.addEventListener('resize', () => {
-					renderer.setSize(window.innerWidth, window.innerHeight);
-					camera.aspect = window.innerWidth / window.innerHeight;
-					camera.updateProjectionMatrix();
-			}, false);
+		const boxSize = 1;
+		geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
+		material = new THREE.MeshNormalMaterial();
+		for(let i = 0; i < fftSize / 2; i++) {
+			box[i] = new THREE.Mesh(geometry, material);
+			box[i].position.set(0, 0, i + i * boxSize / 4);
+			scene.add(box[i]);
+		}
+		axesHelper = new THREE.AxesHelper(5.0);
+		scene.add(axesHelper);
 
-			// rendering
-			render();
+
+		controls = new THREE.OrbitControls(camera, renderer.domElement);
+		// events
+		window.addEventListener('keydown', (eve) => {
+			run = eve.key !== 'Escape';
+			if(eve.key === ' '){
+				isDown = true;
+			}
+		}, false);
+		window.addEventListener('keyup', (eve) => {
+			if(eve.key === ' '){
+				isDown = false;
+			}
+		}, false);
+		window.addEventListener('resize', () => {
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			camera.aspect = window.innerWidth / window.innerHeight;
+			camera.updateProjectionMatrix();
+		}, false);
+
+		// rendering
+		render();
 	}, false);
 
 	// rendering
 	function render(){
-			if(run){requestAnimationFrame(render);}
-			analyser.getFrequencyData();
+		if(run){requestAnimationFrame(render);}
+		analyser.getFrequencyData();
 		uniforms.tAudioData.value.needsUpdate = true;
-			renderer.render(scene, camera);
-			console.log(analyser);
-	}
+		renderer.render(scene, camera);
+		console.log(analyser.data[5]);
+		for(let i = 0; i < fftSize / 2; i++) {
+			box[i].position.y = analyser.data[i] * 0.01;
+		}
+}
 })();
 
