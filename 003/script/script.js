01e357bbd54ae6681f8825847bb92da4c87fb010
mtoutside <more.humor.or@gmail.com>
2019-06-30 16:59:53 +0900

    add camera

--- 003/script/script.js
+++ 003/script/script.js
@@ -20,6 +20,7 @@
     let nowTime;    // 現在までの経過時間
     let gl;         // WebGL Rendering Context
     let mat4;       // glcubic.Math.Mat4 クラス
+		let qtn;
     let canvas;     // canvas エレメントへの参照
     let canvasSize; // canvas の大きさ（ここでは正方形の一辺の長さ）
     let prg;        // プログラムオブジェクト
@@ -28,12 +29,15 @@
     let index;      // 頂点インデックス
     let VBO;        // Vertex Buffer Object
     let IBO;        // Index Buffer Object
+    mat4 = gl3.Math.Mat4;
+    qtn  = gl3.Math.Qtn;
 
     let mMatrix;    // モデル座標変換行列
     let vMatrix;    // ビュー座標変換行列
     let pMatrix;    // プロジェクション座標変換行列
     let vpMatrix;   // ビュー x プロジェクション
     let mvpMatrix;  // モデル x ビュー x プロジェクション
+    let normalMatrix; // 法線変換行列
 
     window.addEventListener('load', () => {
         // glcubic の初期化
@@ -58,6 +62,12 @@
         canvasSize = Math.min(window.innerWidth, window.innerHeight);
         canvas.width  = canvasSize;
         canvas.height = canvasSize;
+
+        // イベントの登録
+        canvas.addEventListener('mousedown', mouseInteractionStart, false);
+        canvas.addEventListener('mousemove', mouseInteractionMove, false);
+        canvas.addEventListener('mouseup', mouseInteractionEnd, false);
+        canvas.addEventListener('wheel', wheelScroll, false);
 
         // シェーダロードへ移行
         loadShader();
@@ -158,6 +168,11 @@
         // プログラムに頂点バッファとインデックスバッファをアタッチ
         prg.setAttribute(VBO, IBO);
 
+        cameraUpdate();
+        mat4.lookAt(cameraPosition, centerPoint, cameraUpDirection, vMatrix);
+        mat4.perspective(45, 1.0, 0.1, cameraDistance * 2.0, pMatrix);
+        mat4.multiply(pMatrix, vMatrix, vpMatrix);
+
         // 時間の経過を得る（Date.now は現在時刻のタイムスタンプをミリ秒で返す）
         nowTime = (Date.now() - startTime) / 1000;
         mat4.identity(mMatrix);
@@ -173,6 +188,79 @@
 
         // 再帰呼び出し
         if(run){requestAnimationFrame(render);}
-    }
+		}
+
+			// カメラ関連 =========================================================
+			let cameraDistance     = 5.0;
+			let cameraPosition     = [0.0, 0.0, cameraDistance];
+			let centerPoint        = [0.0, 0.0, 0.0];
+			let cameraUpDirection  = [0.0, 1.0, 0.0];
+			let dCameraPosition    = [0.0, 0.0, cameraDistance];
+			let dCenterPoint       = [0.0, 0.0, 0.0];
+			let dCameraUpDirection = [0.0, 1.0, 0.0];
+			let cameraRotateX      = 0.0;
+			let cameraRotateY      = 0.0;
+			let cameraScale        = 0.0;
+			let clickStart         = false;
+			let prevPosition       = [0, 0];
+			let offsetPosition     = [0, 0];
+			let qt  = qtn.identity(qtn.create());
+			let qtx = qtn.identity(qtn.create());
+			let qty = qtn.identity(qtn.create());
+			function mouseInteractionStart(eve){
+				clickStart = true;
+				prevPosition = [
+					eve.pageX,
+					eve.pageY
+				];
+				eve.preventDefault();
+			}
+			function mouseInteractionMove(eve){
+				if(!clickStart){return;}
+				let w = canvas.width;
+				let h = canvas.height;
+				let s = 1.0 / Math.min(w, h);
+				offsetPosition = [
+					eve.pageX - prevPosition[0],
+					eve.pageY - prevPosition[1]
+				];
+				prevPosition = [eve.pageX, eve.pageY];
+				switch(eve.buttons){
+					case 1:
+						cameraRotateX += offsetPosition[0] * s;
+						cameraRotateY += offsetPosition[1] * s;
+						cameraRotateX = cameraRotateX % 1.0;
+						cameraRotateY = Math.min(Math.max(cameraRotateY % 1.0, -0.25), 0.25);
+						break;
+				}
+			}
+			function mouseInteractionEnd(eve){
+				clickStart = false;
+			}
+			function wheelScroll(eve){
+				let w = eve.wheelDelta;
+				if(w > 0){
+					cameraScale = 0.8;
+				}else if(w < 0){
+					cameraScale = -0.8;
+				}
+			}
+			function cameraUpdate(){
+				let v = [1.0, 0.0, 0.0];
+				cameraScale *= 0.75;
+				cameraDistance += cameraScale;
+				cameraDistance = Math.min(Math.max(cameraDistance, 5.0), 20.0);
+				dCameraPosition[2] = cameraDistance;
+				qtn.identity(qt);
+				qtn.identity(qtx);
+				qtn.identity(qty);
+				qtn.rotate(cameraRotateX * gl3.PI2, [0.0, 1.0, 0.0], qtx);
+				qtn.toVecIII(v, qtx, v);
+				qtn.rotate(cameraRotateY * gl3.PI2, v, qty);
+				qtn.multiply(qtx, qty, qt)
+				qtn.toVecIII(dCameraPosition, qt, cameraPosition);
+				qtn.toVecIII(dCameraUpDirection, qt, cameraUpDirection);
+			}
+			// カメラ関連ここまで =================================================
 })();
 
